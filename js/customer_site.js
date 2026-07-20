5d2cdab1c0cf54e6904dc4d2e366bdf415af8874
Jeffrey <155841890+Jeffrey20081210@users.noreply.github.com>
2026-07-21 03:09:58 +0800

    Add new customer sites with APIs and names

--- js/customer_site.js
+++ js/customer_site.js
@@ -2,6 +2,18 @@ const CUSTOMER_SITES = {
     qiqi: {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
+    },
+    red_bull: {
+        api: 'http://hongniuzy2.com/api.php/provide/vod/from/hnm3u8',
+        name: '紅牛資源',
+    },
+    quantum: {
+        api: 'http://cj.lziapi.com/api.php/provide/vod/from/lzm3u8',
+        name: '量子資源',
+    },
+    open_cinema: {
+        api: 'http://vod-demo.onrender.com/pubdovod.php',
+        name: '開放電影',
     }
 };
 
